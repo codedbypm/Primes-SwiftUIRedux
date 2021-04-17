c26d2de4d4bc2581c4bd3adf82e79246a6ec7f86
Paolo Moroni <paolo@codedby.pm>
2021-04-17 14:09:22 +0200

    chore: add Xcode project

---
+++ Primes-SwiftUIRedux/Primes-SwiftUIRedux/ContentView.swift
@@ -0,0 +1,19 @@
+// 
+// Project: Primes-SwiftUIRedux 
+// Copyright © 2021 codedby.pm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Primes-SwiftUIRedux/Primes-SwiftUIRedux/Primes_SwiftUIReduxApp.swift
@@ -0,0 +1,15 @@
+// 
+// Project: Primes-SwiftUIRedux 
+// Copyright © 2021 codedby.pm. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Primes_SwiftUIReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
